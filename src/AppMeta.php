99e79d030058ff9ca3ebf747c461b7335d3fc208
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-04-28 11:17:24 +0900

    contextual tmp dir

--- src/AppMeta.php
+++ src/AppMeta.php
@@ -13,13 +13,12 @@ use Koriym\Psr4List\Psr4List;
 class AppMeta extends AbstractAppMeta
 {
     /**
-     * Application name
+     * Application meta
      *
-     * {$Vendor\$Package}
-     *
-     * @param string $name
+     * @param string $name Application name{$Vendor\$Package}
+     * @param string $contexts
      */
-    public function __construct($name)
+    public function __construct($name, $contexts = '')
     {
         $appModule = $name . '\Module\AppModule';
         if (! class_exists($appModule)) {
@@ -27,13 +26,14 @@ class AppMeta extends AbstractAppMeta
         }
         $this->name = $name;
         $this->appDir = dirname(dirname(dirname((new \ReflectionClass($appModule))->getFileName())));
-        $this->tmpDir = $this->appDir . '/var/tmp';
-        if (! is_writable($this->tmpDir)) {
-            throw new NotWritableException($this->tmpDir);
+        $this->tmpDir = $this->appDir . '/var/tmp/'. $contexts;
+        if (! file_exists($this->tmpDir)) {
+            mkdir($this->tmpDir);
         }
         $this->logDir = $this->appDir . '/var/log';
-        if (! is_writable($this->logDir)) {
-            throw new NotWritableException($this->logDir);
+        $isNotProd = strpos($contexts, 'prod') === false;
+        if ($isNotProd) {
+            $this->initForDevelop($this->tmpDir);
         }
     }
 
@@ -43,8 +43,31 @@ class AppMeta extends AbstractAppMeta
     public function getResourceListGenerator()
     {
         $list = new Psr4List;
-        $resourceListGenerator =  $list($this->name . '\Resource', $this->appDir . '/src/Resource');
+        $resourceListGenerator = $list($this->name . '\Resource', $this->appDir . '/src/Resource');
 
         return $resourceListGenerator;
+    }
+
+    /**
+     * @param string $dir
+     */
+    private function initForDevelop($dir)
+    {
+        $unlink = function ($path) use (&$unlink) {
+            foreach (glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $file) {
+                is_dir($file) ? $unlink($file) : unlink($file);
+                @rmdir($file);
+            }
+        };
+        $unlink($dir);
+        if (! is_writable($this->tmpDir)) {
+            throw new NotWritableException($this->tmpDir);
+        }
+        if (! is_writable($this->logDir)) {
+            throw new NotWritableException($this->logDir);
+        }
+        if (function_exists('apc_clear_cache')) {
+            apc_clear_cache('user');
+        }
     }
 }
