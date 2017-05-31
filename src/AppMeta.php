bbbbb0bc3962e43a9d349e4c783c3101d4035526
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-31 13:28:46 +0900

    contextual log dir
    avoid write permission conflicts

--- src/AppMeta.php
+++ src/AppMeta.php
@@ -29,7 +29,10 @@ class AppMeta extends AbstractAppMeta
         if (! file_exists($this->tmpDir) && mkdir($this->tmpDir) && ! is_writable($this->tmpDir)) {
             throw new NotWritableException($this->tmpDir);
         }
-        $this->logDir = $this->appDir . '/var/log';
+        $this->logDir = $this->appDir . '/var/log/' . $context;
+        if (! file_exists($this->logDir) && mkdir($this->logDir) && ! is_writable($this->logDir)) {
+            throw new NotWritableException($this->logDir);
+        }
         $isDevelop = strpos($context, 'prod') === false;
         if ($isDevelop) {
             $this->clearTmpDirectory($this->tmpDir);
