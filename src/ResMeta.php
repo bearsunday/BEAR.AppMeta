26474e921eda676095c19ace72f657f57e6d0a02
tsuchiya <tsuchiya@bengo4.com>
2023-01-06 02:37:38 +0900

    Return only ResourceObject in getResourceListGenerator

--- src/ResMeta.php
+++ src/ResMeta.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace BEAR\AppMeta;
 
+use BEAR\Resource\ResourceObject;
+
 final class ResMeta
 {
     /**
-     * @param class-string $class
+     * @param class-string<ResourceObject> $class
      */
     public function __construct(
         /**
